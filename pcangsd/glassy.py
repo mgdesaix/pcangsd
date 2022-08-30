631f4b44d9d34f5bae6510735cf28e0b8686382c
mgdesaix <mgdesaix@gmail.com>
2022-08-30 09:35:17 -0600

    working on assignment script

---
+++ pcangsd/glassy.py
@@ -0,0 +1,44 @@
+"""
+GLassy addition to pcangsd
+Assign individuals to populations and get mixture proportions
+"""
+
+__author__ = "Matt DeSaix"
+
+# libraries
+import numpy as np
+
+# Import scripts
+from pcangsd import glassy_cy
+
+# log-likelihoods for assignment
+# L is the GLs from beagle file (M x (N*2))
+# A is the allele frequencies of the reference pops (M x K)
+def assignLL(L, A, t):
+    ## Initiate variables and containers
+    # number of loci
+    m = L.shape[0]
+    # number of individuals
+    n = L.shape[1] // 2
+    # number of populations
+    k = A.shape[1]
+    # loglike matrix, of n x k (rows = individuals, columns = reference pops)
+    logl_mat = np.zeros((n,k), dtype=np.float32)
+    
+    for i in range(n):
+        for j in range(k):
+            # set log like vector for individual i to pop j
+            logl_vec = np.zeros(m, dtype=np.float32)
+            # fill vector
+            glassy_cy.loglike(L, A, logl_vec, t, i, j)
+            # loglike sum
+            loglike = np.sum(logl_vec, dtype=float)
+            print("Individual " + int(i) + " done for pop " int(j))
+            print("Log-likelihood: " + str(loglike))
+            # fill output matrix
+            logl_mat[i,j] = loglike
+    del logl_vec
+    return logl_mat
+
+# EM algorithm for mixing proportions
+def emProp():
